3752c6629f64c30dbfbb7809b7e5f244b2caffe2
Matias Karim Iaquinta <116685639+matiasiaquinta@users.noreply.github.com>
2024-08-13 20:05:01 -0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
 export const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+    import.meta.env.VITE_API_URL || "https://sinfronteras-testing-backend.onrender.com";
+    //import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+
