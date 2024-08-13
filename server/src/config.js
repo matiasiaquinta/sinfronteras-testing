185f8ac7d9f4d22580c8a0b172ec3b1092694a1e
Matias Karim Iaquinta <116685639+matiasiaquinta@users.noreply.github.com>
2024-08-13 20:22:00 -0300

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -2,12 +2,12 @@
     process.env.PORT -> es para que detecte el puerto del servidor.
 */
 
-//export const PORT = process.env.PORT || 4000;
-export const PORT = process.env.PORT || "https://sinfronteras-testing-backend.onrender.com";
+export const PORT = process.env.PORT || 4000;
+//export const PORT = process.env.PORT || "https://sinfronteras-testing-backend.onrender.com";
 export const MONGODB_URI =
     process.env.MONGODB_URI || "mongodb://localhost/sinfronteras-prueba";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://sinfronteras-testing-frontend.onrender.com";
 
 //export const TOKEN_SECRET = "some secret key";
