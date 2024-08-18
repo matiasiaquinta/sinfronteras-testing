72b411b595ee951923a36bc7045a10995f4f45d5
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-18 16:39:45 -0300

    updating client config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-export const API_URL =
-    //import.meta.env.VITE_API_URL || "https://sinfronteras-testing-backend.onrender.com";
+/*
+    import.meta.env.VITE_API_URL || "https://sinfronteras-testing-backend.onrender.com";
     import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+*/
+
+export const API_URL = import.meta.env.API_URL;
